94237234f9076591a406ecde4ebc66a4db2038ad
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2019-10-08 14:03:44 -0300

    finished rest client

--- clientes/cliente-rest.rb
+++ clientes/cliente-rest.rb
@@ -1,22 +1,22 @@
 require 'httparty'
 
-puts ("Bienvenido a cliente de servicio REST \n 1)Digito verificador RUT \n 2)Saludo a tu nombre completos")
+puts ("Bienvenido a cliente de servicio REST \n 1)Digito verificador RUT \n 2)Saludo a tu nombre completos \n 0)Salir")
 opcion = gets.chomp.to_i
-
-if opcion == 1
+case opcion 
+when 1
     puts "Ingrese valor del rut sin digito verificador"
-    rut = gets.chomps 
+    rut = gets.chomp
     newrut = "http://127.0.0.1:5000/rut/#{rut}"
     responseRut = HTTParty.get(newrut)
     puts responseRut.body
-elsif opcion == 2
+when 2
     puts "Ingrese primer nombre"
     nom = gets.chomp
     puts "Ingrese apellido paterno"
     pat = gets.chomp
     puts "Ingrese apellido materno"
     mat = gets.chomp
-    puts "Ingrese genero, 0 para ... y 1 para ..."
+    puts "Ingrese genero, 0 para masculino y 1 para femenino"
     gender = gets
     data = HTTParty.post( "http://127.0.0.1:5000/saludo", body:{
         nombre:nom,
@@ -24,5 +24,7 @@ elsif opcion == 2
         materno:mat,
         sexo:gender
     })
-    puts data 
+    puts data         
+when 0
+    puts "adios"
 end
