499bd26920f106f20b0825d8443f1b7b0169a831
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2019-10-08 16:18:20 -0300

    corregir cliente soap digito

--- clientes/cliente-soap.rb
+++ clientes/cliente-soap.rb
@@ -1,6 +1,9 @@
 require 'savon'
 cliente = Savon.client(wsdl: "http://127.0.0.1:8000/?wsdl", pretty_print_xml: true)
+
+puts cliente.operations
 puts "Bienvenido al cliente soap \n 1)verificador rut \n 2)saludo \n 0)salir"
+
 opcion = gets.chomp.to_i
 case opcion
 when 1
@@ -8,7 +11,6 @@ when 1
     message = {rut: '#{rut}'}
     response = cliente.call(:digito_verificador,message:message)
     puts response.doc
-#puts cliente.operations
 when 2
     puts "Ingrese nombre"
     nombre = gets.chomp
@@ -20,6 +22,7 @@ when 2
     sexos = gets.chomp.to_i
     saludo = {nom: nombre, pat: paterno, mat: materno, sexo: sexos}
     respuesta = cliente.call(:generar_saludo, message:saludo)
+    puts respuesta.doc
 when 0
     puts "Adios"
 end
